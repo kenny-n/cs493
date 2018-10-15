8cd80dfbed1ff59c44935ea8a97df606d4f40623
Ken <knnyngo@gmail.com>
2018-10-15 03:03:18 -0700

    added bits about ships and slips

--- week3/server.js
+++ week3/server.js
@@ -4,10 +4,11 @@ const app = express();
 const Datastore = require('@google-cloud/datastore');
 const bodyParser = require('body-parser');
 
-const projectId = 'YOUR-PROJECT-ID';
+const projectId = 'kngo493-003';
 const datastore = new Datastore({projectId:projectId});
 
-const LODGING = "Lodging";
+const SHIP = "Ship";
+const SLIP = "Slip";
 
 const router = express.Router();
 
@@ -18,61 +19,103 @@ function fromDatastore(item){
     return item;
 }
 
-/* ------------- Begin Lodging Model Functions ------------- */
-function post_lodging(name, description, price){
-    var key = datastore.key(LODGING);
-	const new_lodging = {"name": name, "description": description, "price": price};
-	return datastore.save({"key":key, "data":new_lodging}).then(() => {return key});
+/* ------------- Begin Ship Model Functions ------------- */
+function post_ship(name, type, length){
+    var key = datastore.key(SHIP);
+	const new_ship = {"name": name, "type": type, "length": length};
+	return datastore.save({"key":key, "data":new_ship}).then(() => {return key});
 }
 
-function get_lodgings(){
-	const q = datastore.createQuery(LODGING);
-	return datastore.runQuery(q).then( (entities) => {
-			return entities[0].map(fromDatastore);
-		});
+function get_ships(){
+	const q = datastore.createQuery(SHIP);
+	return datastore.runQuery(q).then( (entities) => {return entities[0].map(fromDatastore)});
 }
 
-function put_lodging(id, name, description, price){
-    const key = datastore.key([LODGING, parseInt(id,10)]);
-    const lodging = {"name": name, "description": description, "price": price};
-    return datastore.save({"key":key, "data":lodging});
+function put_ship(id, name, type, length){
+    const key = datastore.key([SHIP, parseInt(id,10)]);
+    const ship = {"name": name, "type": type, "length": length};
+    return datastore.save({"key":key, "data":ship});
 }
 
-function delete_lodging(id){
-    const key = datastore.key([LODGING, parseInt(id,10)]);
+function delete_ship(id){
+    const key = datastore.key([SHIP, parseInt(id,10)]);
     return datastore.delete(key);
 }
+/* ------------- End Ship Model Functions ------------- */
 
-/* ------------- End Model Functions ------------- */
+/* ------------- Begin Slip Model Functions ------------- */
+function post_slip(name, type, length){
+    var key = datastore.key(SLIP);
+	const new_slip = {"number": number, "current_boat": current_boat, "arrival_date": arrival_date};
+	return datastore.save({"key":key, "data":new_slip}).then(() => {return key});
+}
 
-/* ------------- Begin Controller Functions ------------- */
+function get_slips(){
+	const q = datastore.createQuery(SLIP);
+	return datastore.runQuery(q).then( (entities) => {return entities[0].map(fromDatastore)});
+}
 
-router.get('/', function(req, res){
-    const lodgings = get_lodgings()
-	.then( (lodgings) => {
-        console.log(lodgings);
-        res.status(200).json(lodgings);
+function put_slip(id, name, type, length){
+    const key = datastore.key([SLIP, parseInt(id,10)]);
+    const slip = {"number": number, "current_boat": current_boat, "arrival_date": arrival_date};
+    return datastore.save({"key":key, "data":slip});
+}
+
+function delete_slip(id){
+    const key = datastore.key([SLIP, parseInt(id,10)]);
+    return datastore.delete(key);
+}
+/* ------------- End Slip Model Functions ------------- */
+
+/* ------------- Begin Ship Controller Functions ------------- */
+router.get('/ships', function(req, res){
+    const ships = get_ships()
+	.then( (ships) => {
+        console.log(ships);
+        res.status(200).json(ships);
     });
 });
 
-router.post('/', function(req, res){
+router.post('/ships', function(req, res){
     console.log(req.body);
-    post_lodging(req.body.name, req.body.description, req.body.price)
+    post_ship(req.body.name, req.body.type, req.body.length)
     .then( key => {res.status(200).send('{ "id": ' + key.id + ' }')} );
 });
 
-router.put('/:id', function(req, res){
-    put_lodging(req.params.id, req.body.name, req.body.description, req.body.price)
-    .then(res.status(200));
+router.put('/ships/:id', function(req, res){
+    put_ship(req.params.id, req.body.name, req.body.type, req.body.length)
+    .then(res.status(200).end());
 });
 
-router.delete('/:id', function(req, res){
-    delete_lodging(req.params.id).then(res.status(200).end())
+router.delete('/ships/:id', function(req, res){
+    delete_ship(req.params.id).then(res.status(200).end())
+});
+/* ------------- End Ship Controller Functions ------------- */
+
+/* ------------- Begin Slip Controller Functions ------------- */
+router.get('/slips', function(req, res){
+    const slips = get_slips()
+	.then( (slips) => {
+        console.log(slips);
+        res.status(200).json(slips);
+    });
 });
 
-/* ------------- End Controller Functions ------------- */
+router.post('/slips', function(req, res){
+    console.log(req.body);
+    post_slip(req.body.number, req.body.current_boat, req.body.arrival_date)
+    .then( key => {res.status(200).send('{ "id": ' + key.id + ' }')} );
+});
 
-app.use('/lodgings', router);
+router.put('/slips/:id', function(req, res){
+    put_slip(req.params.id, req.body.number, req.body.current_boat, req.body.arrival_date)
+    .then(res.status(200).end());
+});
+
+router.delete('/slips/:id', function(req, res){
+    delete_slip(req.params.id).then(res.status(200).end())
+});
+/* ------------- End Slip Controller Functions ------------- */
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
